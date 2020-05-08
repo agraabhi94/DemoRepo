5a79827e2fd68508eae97728e38bc9596427250c
Abhishek Agrawal <agra.abhi94@gmail.com>
2020-05-08 12:19:38 +0530

    Add first code

---
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -0,0 +1,14 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	
+@Test	
+public void firstTest() {
+		
+		System.out.println("First test case");
+	}
+
+}
